8493fc7871e17836b50b03aa6bce2a78870e6676
MUSHLIHUN <mushlihun@asiapay.com>
2023-07-17 18:01:10 +0700

    update

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -8,8 +8,21 @@ class About extends MY_Controller {
 
 	public function index()
 	{
-	    // var_dump($this);
-	    // exit;
+		$id_taruna = isset($_GET['id_taruna']) ? $_GET['id_taruna'] : null;
+	    $this->db->select('*')->from('taruna');
+        $this->db->where('id', 1);
+        $mk = $this->db->get();
+        $this->mViewData['data_taruna'] = $mk->row();
+        $nama_taruna = $mk->row()->nama;
+		$tempat_lahir = $mk->row()->tempat_lahir;
+		$tanggal_lahir = $mk->row()->tanggal_lahir;
+
+		$this->db->select('*')->from('kota');
+        $this->db->where('id', $tempat_lahir);
+        $kota = $this->db->get();
+		
+		$ttl = $kota->row()->nama;
+        $this->mViewData['data_tmpt'] = $kota->row();
 		$this->render('about', 'empty');
 	}
 }
--- application/views/about.php
+++ application/views/about.php
@@ -1,18 +1,47 @@
-<div class="services">
-<div class="container">
-        <h3>Cetak Ijazah</h3>
-        <hr>
-</div>
 <div class="container" align="right" class="print-screen">
     <button onClick="print()" class="btn btn-primary btn-lg print-screen">Cetak</button>
 </div>
-        <div class="col-md-6 wow fadeInDown animated" data-wow-duration="1000ms" data-wow-delay="300ms" style="visibility: visible;-webkit-animation-duration: 1000ms; -moz-animation-duration: 1000ms; animation-duration: 1000ms;-webkit-animation-delay: 300ms; -moz-animation-delay: 300ms; animation-delay: 300ms;">
-            <h2>Cetak Ijazah</h2>
-            <div class="container" align="right" class="print-screen">
-        </div>
-    </div>
+<div class="services">
+    <div class="container" align="center" class="print-screen">
+    <style>
+body, h1, h2, h3, h4, h5, h6, td  {
+  font-family: "Bookman Old Style" ;
+}
+.table.siswa>tbody>tr>td {
+    border-top:none;
+}
+</style>
+    <h3 style="text-transform: uppercase;font-size: 26px;"><b>IJAZAH<br></b></h3>
+<?php
+    // DATA MATA KULIAH
+    $nama_taruna = isset($data_taruna->nama) ? $data_taruna->nama : '';
+    $tl = isset($data_tmpt->tempat_lahir) ? $data_tmpt->tempat_lahir : '';
+    $tanggal_lahir = isset($data_taruna->tanggal_lahir) ? $data_taruna->tanggal_lahir : '';
+?>
+    <div class="col-sm-9" id="data">
+    <table class="table siswa">
+    <tbody style="font-size: 11;">
+        <tr> 
+            <td>Memberikan Ijazah Kepada</td>
+            <td width="5px">:</td>
+            <td><?=$nama_taruna?></td>
+        </tr>
+        <tr>
+            <td>Tempat dan Tanggal Lahir</td>
+            <td width="5px">:</td>
+            <td><?=$tl?>asd</td>
+            <td><?=$tanggal_lahir?></td>
+        </tr>
+        <tr>
+            <td>Memberikan Ijazah Kepada</td>
+            <td width="5px">:</td>
+            <td><?=$nama_taruna?></td>
+        </tr>
+        <tr>
+            <td>Tempat dan Tanggal Lahir</td>
+            <td width="5px">:</td>
+            <td><?=$tl?>asd</td>
+        </tr>
+    </tbody>
+    </table>
 </div>
-<body onload="window.print()">
-<div class="container isi">
-</div>
-</body>
