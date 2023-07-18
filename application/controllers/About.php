188a60e4dc8d1bbe56fe873543aa39427510da5f
MUSHLIHUN <mushlihun@asiapay.com>
2023-07-18 12:08:36 +0700

    update ijazah

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -5,24 +5,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * About page
  */
 class About extends MY_Controller {
-
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('M_ijazah');
+    }
+	private static $table = 'ijazah';
 	public function index()
 	{
-		$id_taruna = isset($_GET['id_taruna']) ? $_GET['id_taruna'] : null;
-	    $this->db->select('*')->from('taruna');
-        $this->db->where('id', 1);
-        $mk = $this->db->get();
-        $this->mViewData['data_taruna'] = $mk->row();
-        $nama_taruna = $mk->row()->nama;
-		$tempat_lahir = $mk->row()->tempat_lahir;
-		$tanggal_lahir = $mk->row()->tanggal_lahir;
-
+		$id_ijazah = isset($_GET['id_ijazah']) ? $_GET['id_ijazah'] : 1;//null;
+		$data['ijazah'] = $this->M_ijazah->get_ijazah($id_ijazah);
+	    $this->db->select('*')->from('ijazah');
+        $this->db->where('id', $id_ijazah);
+        $data = $this->db->get();
+        $this->mViewData['data_ijazah'] = $data->row();
+		$id_taruna = $data->row()->taruna;
+		$id_direk = $data->row()->direktur;
+		$id_wadirek = $data->row()->wakil_direktur;
+		$this->db->select('*')->from('taruna');
+        $this->db->where('id', $id_taruna);
+        $data = $this->db->get();
+        $this->mViewData['data_taruna'] = $data->row();
+		$tempat_lahir = $data->row()->tempat_lahir;
+		$prodi = $data->row()->program_studi;
 		$this->db->select('*')->from('kota');
         $this->db->where('id', $tempat_lahir);
         $kota = $this->db->get();
-		
-		$ttl = $kota->row()->nama;
-        $this->mViewData['data_tmpt'] = $kota->row();
+        $this->mViewData['data_kota'] = $kota->row();
+		$this->db->select('*')->from('program_studi');
+        $this->db->where('id', $prodi);
+        $data_prodi = $this->db->get();
+        $this->mViewData['data_prodi'] = $data_prodi->row();
+		$this->db->select('*')->from('pejabat');
+        $this->db->where('id', $id_direk);
+        $data_pejabat = $this->db->get();
+        $this->mViewData['data_pejabat'] = $data_pejabat->row();
 		$this->render('about', 'empty');
 	}
 }
--- application/views/about.php
+++ application/views/about.php
@@ -7,6 +7,7 @@
 body, h1, h2, h3, h4, h5, h6, td  {
   font-family: "Bookman Old Style" ;
 }
+.table{margin-left: 15%;}
 .table.siswa>tbody>tr>td {
     border-top:none;
 }
@@ -14,34 +15,80 @@ body, h1, h2, h3, h4, h5, h6, td  {
     <h3 style="text-transform: uppercase;font-size: 26px;"><b>IJAZAH<br></b></h3>
 <?php
     // DATA MATA KULIAH
+    $no_sk = isset($data_ijazah->nomor_sk) ? $data_ijazah->nomor_sk : '';
     $nama_taruna = isset($data_taruna->nama) ? $data_taruna->nama : '';
-    $tl = isset($data_tmpt->tempat_lahir) ? $data_tmpt->tempat_lahir : '';
+    $ttl = isset($data_kota->nama) ? $data_kota->nama : '';
     $tanggal_lahir = isset($data_taruna->tanggal_lahir) ? $data_taruna->tanggal_lahir : '';
+    $no_taruna = isset($data_taruna->nomor_taruna) ? $data_taruna->nomor_taruna : '';
+    $propen = isset($data_prodi->program_pendidikan) ? $data_prodi->program_pendidikan : '';
+    $prodi = isset($data_prodi->nama_prodi) ? $data_prodi->nama_prodi : '';
+    $direk = isset($data_pejabat->nama) ? $data_pejabat->nama : '';
+    $wadirek = isset($data_pejabat->nama) ? $data_pejabat->nama : '';
 ?>
-    <div class="col-sm-9" id="data">
+    <div class="col-sm-12" id="data">
     <table class="table siswa">
     <tbody style="font-size: 11;">
         <tr> 
             <td>Memberikan Ijazah Kepada</td>
             <td width="5px">:</td>
-            <td><?=$nama_taruna?></td>
+            <td style="font-weight:bold"><?=$nama_taruna?></td>
         </tr>
         <tr>
             <td>Tempat dan Tanggal Lahir</td>
             <td width="5px">:</td>
-            <td><?=$tl?>asd</td>
-            <td><?=$tanggal_lahir?></td>
+            <td><?=$ttl?>, <?=$tanggal_lahir?></td>
         </tr>
         <tr>
-            <td>Memberikan Ijazah Kepada</td>
+            <td>No Taruna</td>
             <td width="5px">:</td>
-            <td><?=$nama_taruna?></td>
+            <td><?=$no_taruna?></td>
         </tr>
         <tr>
-            <td>Tempat dan Tanggal Lahir</td>
+            <td>Program Pendidikan</td>
             <td width="5px">:</td>
-            <td><?=$tl?>asd</td>
+            <td><?=$propen?></td>
+        </tr>
+        <tr>
+            <td>Program Studi</td>
+            <td width="5px">:</td>
+            <td><?=$prodi?></td>
+        </tr>
+        <tr>
+            <td>Status</td>
+            <td width="5px">:</td>
+            <td>TERAKREDITASI "BAIK"<br><p style="font-style:italic;">Berdasarkan Keputusan BAN-PT No.321</p></td>
         </tr>
     </tbody>
     </table>
 </div>
+<div class="col-md-12 wow fadeInDown animated" data-wow-duration="1000ms" data-wow-delay="600ms" style="visibility: visible;-webkit-animation-duration: 1000ms; -moz-animation-duration: 1000ms; animation-duration: 1000ms;-webkit-animation-delay: 600ms; -moz-animation-delay: 600ms; animation-delay: 600ms; text-align: justify;">
+    <p>Ijazah ini diserahkan berdasarkan Surat Keputusan Direktur Politeknik XYZ Nomor : <?=$no_sk?> Tahun 2023 setelah yang
+bersangkutan memenuhi semua persyaratan yang telah ditentukan dan kepadanya dilimpahkan segala wewenang dan hak yang
+berhubungan dengan Ijazah yang dimilikinya serta berhak menggunakan Gelar Akademik <span style="font-weight:bold;"> Ahli Madya Komputer (A.Md)</span>.</p>
+    </div>
+    <div class="col-sm-9" id="data">
+    <table class="table siswa">
+    <tbody style="font-size: 11; text-align:center;">
+        <tr>
+        <td></td>    
+        <td>Jakarta, 29 Juli 2023</td>
+        </tr>
+        <tr>
+            <td>WAKIL DIREKTUR</td>
+            <td>DIREKTUR</td>
+        </tr>
+        <tr style="height:100px">
+            <td>Politeknik XYZ</td>
+            <td>Politeknik XYZ</td>
+        </tr>
+        <tr>
+            <td><?=$direk?></td>
+            <td><?=$wadirek?></td>
+        </tr>
+        <tr>
+            <td>NIP</td>
+            <td>NIP</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
